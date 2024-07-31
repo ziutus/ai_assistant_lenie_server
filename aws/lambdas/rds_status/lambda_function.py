cefbaefa2c0c6dc275d46951be56b69c1910aa03
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2024-07-31 08:38:12 +0200

    Add RDS status Lambda function and deployment script
    This commit introduces a new Lambda function to check the status of an RDS instance based on its ID, with responses tailored for CORS and error handling. Additionally, a deployment shell script is added to package and update the Lambda function using AWS CLI.

---
+++ aws/lambdas/rds_status/lambda_function.py
@@ -0,0 +1,43 @@
+import json
+import boto3
+import os
+from pprint import pprint
+
+client = boto3.client("rds")
+db_id = os.environ.get('DB_ID')
+#db_id="lenie-dev"
+
+def lambda_handler(event, context):
+    try:
+        response = client.describe_db_instances(DBInstanceIdentifier=db_id)
+
+        if len(response['DBInstances']) != 1:
+            return {
+            'headers': {
+                  'Access-Control-Allow-Origin': '*',
+                  'Access-Control-Allow-Credentials': True,
+                },
+                'statusCode': 500,
+                'body': json.dumps(f'Error during checking status of database, wrong number of DB instances! >{len(response["DBInstances"])}')
+            }
+
+        return {
+            'statusCode': 200,
+            'body': response['DBInstances'][0]['DBInstanceStatus'],
+            'headers': {
+                  'Access-Control-Allow-Origin': '*',
+                  'Access-Control-Allow-Credentials': True,
+                  'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+        }
+
+    except Exception as e:
+        pass
+        return {
+            'headers': {
+                  'Access-Control-Allow-Origin': '*',
+                  'Access-Control-Allow-Credentials': True,
+            },
+            'statusCode': 500,
+            'body': json.dumps(f'Error during checking status of database >{db_id} {str(e)}')
+        }
