eec2f4befde79eeb5b3a57ddfc1170acc005af25
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2024-07-31 08:48:22 +0200

    Rename Lambda function in update script
    Changed the AWS Lambda function name from 'lenie-rds-status' to 'lenie-rds-stop' in lambda_update.sh script. This ensures the correct function is updated during the deployment process.

--- aws/lambdas/rds_stop/lambda_update.sh
+++ aws/lambdas/rds_stop/lambda_update.sh
@@ -5,6 +5,6 @@ PROFILE="lenie-ai-admin"
 zip -r lambda.zip lambda_function.py
 
 
-aws lambda update-function-code --function-name lenie-rds-status  --zip-file fileb://lambda.zip --profile ${PROFILE}
+aws lambda update-function-code --function-name lenie-rds-stop  --zip-file fileb://lambda.zip --profile ${PROFILE}
 
 rm lambda.zip
