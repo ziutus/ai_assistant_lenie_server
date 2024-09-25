2f9789dcb4761c2b836841d0222e3998ec3f729b
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2024-09-25 09:07:47 +0200

    Simplify lambda imports by removing unused modules
    Removed several unused import statements from the lambda_function.py file, including StalkerWebDocumentDB, WebsitesDBPostgreSQL, chapters_text_to_list, website_is_paid, and split_text_for_embedding. This change streamlines the code and enhances readability.

--- aws/lambdas/lenie-server-internet/lambda_function.py
+++ aws/lambdas/lenie-server-internet/lambda_function.py
@@ -5,13 +5,8 @@ import logging
 from urllib.parse import parse_qs
 
 import library.ai
-from library.stalker_web_document_db import StalkerWebDocumentDB
-from library.stalker_web_documents_db_postgresql import WebsitesDBPostgreSQL
-from library.text_transcript import chapters_text_to_list
 from library.translate import text_translate
 from library.website.website_download_context import download_raw_html, webpage_raw_parse, WebPageParseResult
-from library.website.website_paid import website_is_paid
-from library.text_functions import split_text_for_embedding
 from library.embedding import get_embedding
 
 logging.basicConfig(level=logging.DEBUG)  # Change level as per you r need
