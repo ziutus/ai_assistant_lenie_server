2b9469ed9eefecee57a3315af465d70340cbefa1
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2024-10-30 15:06:51 +0100

    Rename test method and improve import structure
    Renamed the test method to better reflect its intent. Also refactored import statements to enhance readability and maintainability.

--- tests/unit/test_website_is_paid.py
+++ tests/unit/test_website_is_paid.py
@@ -1,11 +1,8 @@
 import unittest
-import sys
 import os
+from server import app
 
 os.environ['DEBUG'] = 'false'
-
-sys.path.append("library")
-from server import app
 
 
 class FlaskTestCase(unittest.TestCase):
@@ -14,8 +11,8 @@ class FlaskTestCase(unittest.TestCase):
         app.testing = True
         self.client = app.test_client()
 
-    def test_hello_world(self):
-        data = { "url": "https://www.google.com/" }
+    def test_google_is_paid(self):
+        data = {"url": "https://www.google.com/"}
         response = self.client.post('/website_is_paid', data=data)
         self.assertEqual(response.status_code, 200)
         response_data = response.get_json()
