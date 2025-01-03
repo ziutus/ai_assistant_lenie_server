774bf225cd7ecaac92c45d68459dca4e295a483d
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2025-01-03 09:51:55 +0100

    Add new OpenAI client methods with observation decorators
    Introduced `get_completion2` and `get_completion_image` methods to enhance functionality. Integrated `@observe` decorators to enable monitoring and logging. These updates allow handling of richer prompt inputs and improved interactions with OpenAI APIs.

--- library/api/openai/openai_my.py
+++ library/api/openai/openai_my.py
@@ -1,6 +1,9 @@
 import os
+import json
 
 from openai import OpenAI
+from langfuse.decorators import observe
+from langfuse.openai import openai
 
 
 class OpenAIClient:
@@ -9,6 +12,7 @@ class OpenAIClient:
     Provides a method to obtain completions.
     """
 
+    @observe()
     @staticmethod
     # model="gpt-4"
     # model="gpt-3.5-turbo"
@@ -48,3 +52,53 @@ class OpenAIClient:
 
         except Exception as e:
             raise Exception(f"An error occurred: {e}")
+
+    @observe()
+    def get_completion2(prompt: str, model: str = "gpt-4o-mini") -> str:
+        client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+        response = client.chat.completions.create(
+            model=model,
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": prompt},
+
+                    ],
+                }
+            ],
+            max_tokens=1000,
+        )
+
+        return json.loads(response.choices[0].message.content)
+
+    @observe()
+    def get_completion_image(prompt: str, image_urls=[], detail: str = "auto", model: str = "gpt-4o-mini", max_tokens = 300) -> str:
+        client = OpenAI()
+
+        content = [{"type": "text", "text": prompt}]
+
+        for image in image_urls:
+            content.append(
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": image,
+                        "detail": detail
+                    }
+                }
+        )
+
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {
+                    "role": "user",
+                    "content": content,
+                }
+            ],
+            max_tokens= max_tokens,
+        )
+
+        return response.choices[0].message.content
