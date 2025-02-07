09b636e9197cc550cd6b14d05870758702d089b2
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2025-02-07 17:07:50 +0100

    Refactor cache handling and data processing logic
    Updated file operations to use proper encoding and improved cache handling with `with` statements. Simplified and clarified the data analysis block for better readability. This enhances code maintainability and reduces potential file handling errors.

--- ai_dev3/serper_dev.py
+++ ai_dev3/serper_dev.py
@@ -16,29 +16,34 @@ os.makedirs(cache_dir, exist_ok=True)
 
 if os.path.isfile(cache_filename):
   print("File already exists in cache")
-  data = json.load(open(cache_filename))
-  pprint(data)
-  exit()
+  with open(cache_filename, "r", encoding="utf-8") as file:
+    data = json.loads(file.read())
+else:
+  conn = http.client.HTTPSConnection("google.serper.dev")
+  payload = json.dumps({
+    "q": query
+  })
+  headers = {
+    'X-API-KEY': os.environ.get('SERPER_DEV_APIKEY'),
+    'Content-Type': 'application/json'
+  }
 
+  conn.request("POST", "/search", payload, headers)
+  res = conn.getresponse()
+  data = res.read()
+  pprint(f"MD5 hash of the query: {query_md5}")
 
-conn = http.client.HTTPSConnection("google.serper.dev")
-payload = json.dumps({
-  "q": query
-})
-headers = {
-  'X-API-KEY': os.environ.get('SERPER_DEV_APIKEY'),
-  'Content-Type': 'application/json'
-}
+  with open(cache_filename, "w", encoding="utf-8") as file:
+    file.write(data.decode("utf-8"))
+    print(f"File saved in cache {cache_filename}")
+    data = json.loads(data)
 
-
-
-conn.request("POST", "/search", payload, headers)
-res = conn.getresponse()
-data = res.read()
-pprint(data.decode("utf-8"))
-pprint(f"MD5 hash of the query: {query_md5}")
-
-with open(cache_filename, "w", encoding="utf-8") as file:
-  file.write(data.decode("utf-8"))
-  print(f"File saved in cache {cache_filename}")
+print("Analizyng data from google")
+for item in data["organic"]:
+  print ("---")
+  print(item["position"])
+  print(item["title"])
+  print(item["snippet"])
+  print(item["link"])
+  # pprint(item)
 
