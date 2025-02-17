7bd8d938cfe7d42db21a640de79aa4d6da5906f8
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2025-02-17 16:06:57 +0100

    Remove `website_put_into_rds` Lambda function and scripts.
    This commit deletes the Python Lambda function and its associated shell script for updating the AWS Lambda. The functionality appears to be deprecated or no longer required. Removing these files helps in cleaning up unused resources.

--- infra/aws/serverless/lambdas/website_put_into_rds/lambda_update.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-PROFILE="lenie-ai-admin"
-
-zip -r lambda.zip lambda_function.py
-
-
-aws lambda update-function-code --function-name lenie-webpage-put-link-rds  --zip-file fileb://lambda.zip  --profile ${PROFILE}
