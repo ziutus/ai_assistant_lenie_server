c84435dcf4fbe152f43fcfa809e44b33a51c3c83
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2025-02-20 16:43:50 +0100

    Refactor S3 upload logic and improve logging.
    Replaced hardcoded bucket name with an environment variable. Added support for uploading HTML content to S3 and improved error handling. Switched from print statements to structured logging for better observability.

--- infra/aws/serverless/lambdas/weblink_put_into_sqs/lambda_function.py
+++ infra/aws/serverless/lambdas/weblink_put_into_sqs/lambda_function.py
@@ -1,9 +1,9 @@
 import json
-import logging
 import os
+import boto3
 import uuid
 
-import boto3
+import logging
 
 logger = logging.getLogger()
 logger.setLevel("INFO")
@@ -15,27 +15,31 @@ def lambda_handler(event, context):
     sqs = boto3.client('sqs')
     s3 = boto3.client('s3')
     queue_url = os.getenv("AWS_QUEUE_URL_ADD")
-    bucket_name = "lenie-s3-tmp"
+    bucket_name = os.getenv("BUCKET_NAME")
 
     url_data = json.loads(event["body"])
     url_data_print = json.loads(event["body"])
     url_data_print["text"] = url_data_print["text"][:50]
+    url_data_print["html"] = url_data_print["html"][:50]
 
     logger.info('data which came by API gateway', extra=url_data_print)
 
-    # Pobierz wartości z url_data
     target_url = url_data.get("url")
     url_type = url_data.get("type")
-    source = url_data.get("source", "default_source")
     note = url_data.get("note", "default_note")
     text = url_data.get("text", "")
+    html = url_data.get("html", "")
     title = url_data.get("title", "")
     language = url_data.get("language", "")
+    paywall = url_data.get("paywall", False)
+    source = url_data.get("source", "own")
+    ai_summary = url_data.get("ai_summary", False)
+    ai_correction = url_data.get("ai_correction", False)
+    chapter_list = url_data.get("chapter_list", False)
 
-    # Sprawdź, czy wartości wymagane są obecne
     if not target_url or not url_type:
         error_message = "Missing required parameter(s): 'url' or 'type'"
-        print(error_message)
+        logger.error(error_message)
         return {
             'statusCode': 500,
             'body': json.dumps(error_message),
@@ -45,36 +49,70 @@ def lambda_handler(event, context):
             },
         }
 
-    # Generowanie losowego UID
-    uid = str(uuid.uuid4())
+    if url_type == 'webpage':
+        uid = str(uuid.uuid4())
 
-    # Tworzenie pliku z wartością zmiennej 'text' i wysyłanie go do S3
-    file_name = f"{uid}.txt"
-    try:
-        s3.put_object(Bucket=bucket_name, Key=file_name, Body=text)
-        print(f"Successfully uploaded {file_name} to {bucket_name}")
-    except Exception as e:
-        error_message = f"Failed to upload {file_name} to {bucket_name}: {str(e)}"
-        print(error_message)
-        return {
-            'statusCode': 500,
-            'body': json.dumps(error_message),
-            'headers': {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Credentials': True,
-            },
+        file_name = f"{uid}.txt"
+        try:
+            s3.put_object(Bucket=bucket_name, Key=file_name, Body=text)
+            logger.info(f"Successfully uploaded {file_name} to {bucket_name}")
+        except Exception as e:
+            error_message = f"Failed to upload {file_name} to {bucket_name}: {str(e)}"
+            logger.error(error_message)
+            return {
+                'statusCode': 500,
+                'body': json.dumps(error_message),
+                'headers': {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': True,
+                },
+            }
+
+        if not html:
+            logger.info("Missing HTML part!")
+        else:
+            file_name = f"{uid}.html"
+            try:
+                s3.put_object(Bucket=bucket_name, Key=file_name, Body=html)
+                logger.info(f"Successfully uploaded {file_name} to {bucket_name}")
+            except Exception as e:
+                error_message = f"Failed to upload {file_name} to {bucket_name}: {str(e)}"
+                logger.error(error_message)
+                return {
+                    'statusCode': 500,
+                    'body': json.dumps(error_message),
+                    'headers': {
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Credentials': True,
+                    },
+                }
+
+        message_body = {
+            "url": target_url,
+            "type": "webpage",
+            "source": source,
+            "note": note,
+            "title": title,
+            "language": language,
+            "paywall": paywall,
+            "ai_summary": ai_summary,
+            "ai_correction": ai_correction,
+            "chapter_list": chapter_list,
+            "s3_uuid": uid
         }
-
-    # Utwórz message_body bez 'text'
-    message_body = {
-        "url": target_url,
-        "type": url_type,
-        "source": source,
-        "note": note,
-        "title": title,
-        "language": language,
-        "s3_uuid": uid  # Dodanie UID do message_body
-    }
+    else:
+        message_body = {
+            "url": target_url,
+            "type": url_type,
+            "source": source,
+            "note": note,
+            "title": title,
+            "language": language,
+            "paywall": paywall,
+            "ai_summary": ai_summary,
+            "ai_correction": ai_correction,
+            "chapter_list": chapter_list,
+        }
 
     response = sqs.send_message(
         QueueUrl=queue_url,
@@ -83,7 +121,7 @@ def lambda_handler(event, context):
     )
 
     if response["ResponseMetadata"]['HTTPStatusCode'] != 200:
-        print("Failed to send message to SQS")
+        logger.error("Failed to send message to SQS")
         return {
             'statusCode': 500,
             'body': json.dumps("Failed to send message to SQS"),
@@ -93,7 +131,7 @@ def lambda_handler(event, context):
             },
         }
 
-    print("Successfully sent message to SQS, message ID: ", response["MessageId"])
+    logger.info(f"Successfully sent message to SQS, message ID: {response['MessageId']}")
 
     return {
         'statusCode': 200,
