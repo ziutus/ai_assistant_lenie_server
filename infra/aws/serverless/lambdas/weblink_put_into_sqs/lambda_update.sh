60e3bbf3ed3b71a0a7cad2cece85b023fd90a25f
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2025-02-21 16:30:20 +0100

    Update lambda deployment to use S3 for code uploads
    Replaced direct Lambda code update with S3 upload for deployment. This allows for better integration with CloudFormation and aligns with best practices for managing Lambda code.

--- infra/aws/serverless/lambdas/weblink_put_into_sqs/lambda_update.sh
+++ infra/aws/serverless/lambdas/weblink_put_into_sqs/lambda_update.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
 PROFILE="lenie-ai-admin"
-FUNCTION_NAME="lenie-url-add"
+FUNCTION_NAME_PART="url-add"
+PROJECT="lenie"
+STAGE="dev"
 
 zip -r lambda.zip lambda_function.py
 
+aws s3 cp lambda.zip s3://${PROJECT}-${STAGE}-cloudformation/${PROJECT}-${STAGE}-${FUNCTION_NAME_PART}.zip --profile ${PROFILE}
 
-aws lambda update-function-code --function-name ${FUNCTION_NAME}  --zip-file fileb://lambda.zip --profile ${PROFILE}
+#aws lambda update-function-code --function-name ${FUNCTION_NAME}  --zip-file fileb://lambda.zip --profile ${PROFILE}
 
 rm lambda.zip
