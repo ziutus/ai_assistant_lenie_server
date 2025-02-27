2671b9a08513a184772fbfc57400e0efec40ea2e
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2025-02-27 17:21:39 +0100

    Add AWS credential variables to Terraform configuration
    Introduced `AWS_ACCOUNT_ID`, `AWS_SECRET_ACCESS_KEY`, and `AWS_ACCESS_KEY_ID` as Terraform variables with defaults. These changes ensure sensitive credentials can be managed securely and consistently across environments. Set the `AWS_SECRET_ACCESS_KEY` variable as sensitive to enhance security.

--- infra/aws/terraform/variables.tf
+++ infra/aws/terraform/variables.tf
@@ -1,1 +1,16 @@
+variable "AWS_ACCOUNT_ID" {
+  description = "The ID of the working account"
+  type = string
+  default = "none"
+}
 
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+  default = "none"
+  sensitive = true
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+  default = "none"
+}
