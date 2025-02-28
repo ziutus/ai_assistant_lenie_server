5aed13f2ad8d1fb5873363dca3d86a22a798d3ae
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2025-02-28 13:52:43 +0100

    Fix admin IP format for NAT Gateway in Terraform config
    Updated the admin_ip variable to ensure it uses CIDR notation by appending "/32". This resolves potential issues with IP address validation during NAT Gateway deployment.

--- infra/aws/terraform/ec2-nat-gateway.tf
+++ infra/aws/terraform/ec2-nat-gateway.tf
@@ -3,7 +3,7 @@ module "ec2-nat-gateway" {
   vpc_id = aws_vpc.lenie_vpc.id
   subnet_id = aws_subnet.subnet-public[0].id
   environment = var.environment
-  admin_ip = var.admin_ip
+  admin_ip = "${var.admin_ip}/32"
   ami = data.aws_ami.al2023-ami.id
   name = "${var.project}-${lower(var.environment)}-nat-gateway"
   instance_type = "t2.micro"
