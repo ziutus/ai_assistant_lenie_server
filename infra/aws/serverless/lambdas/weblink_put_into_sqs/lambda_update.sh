e22a6c32d12bd05a5d1864c8d1fc096c0e1f2eba
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2025-07-31 08:00:09 +0200

    Refactor `weblink_put_into_sqs` Lambda update script for environment variable support
    - Sourced `env.sh` for shared configuration.
    - Replaced hardcoded values with `${PROJECT_NAME}` and `${ENVIRONMENT}`.

--- infra/aws/serverless/lambdas/weblink_put_into_sqs/lambda_update.sh
+++ infra/aws/serverless/lambdas/weblink_put_into_sqs/lambda_update.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
 
-PROFILE="lenie-ai-admin"
+source ../../env.sh
+
 FUNCTION_NAME_PART="url-add"
-PROJECT="lenie"
-STAGE="dev"
 
 zip -r lambda.zip lambda_function.py
 
-aws s3 cp lambda.zip s3://${PROJECT}-${STAGE}-cloudformation/${PROJECT}-${STAGE}-${FUNCTION_NAME_PART}.zip --profile ${PROFILE}
+aws s3 cp lambda.zip s3://${PROJECT_NAME}-${ENVIRONMENT}-cloudformation/${PROJECT_NAME}-${ENVIRONMENT}-${FUNCTION_NAME_PART}.zip --profile ${PROFILE}
 
 #aws lambda update-function-code --function-name ${FUNCTION_NAME}  --zip-file fileb://lambda.zip --profile ${PROFILE}
 
