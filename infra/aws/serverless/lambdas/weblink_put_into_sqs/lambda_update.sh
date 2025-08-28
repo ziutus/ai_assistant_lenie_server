7f4a2b42e4fea1426045455f72ee63767261c179
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2025-08-28 09:02:06 +0200

    Update `lambda_update.sh` to use `AWS_S3_BUCKET_NAME` for dynamic S3 bucket configuration

--- infra/aws/serverless/lambdas/weblink_put_into_sqs/lambda_update.sh
+++ infra/aws/serverless/lambdas/weblink_put_into_sqs/lambda_update.sh
@@ -6,7 +6,7 @@ FUNCTION_NAME_PART="url-add"
 
 zip -r lambda.zip lambda_function.py
 
-aws s3 cp lambda.zip s3://${PROJECT_NAME}-${ENVIRONMENT}-cloudformation/${PROJECT_NAME}-${ENVIRONMENT}-${FUNCTION_NAME_PART}.zip --profile ${PROFILE}
+aws s3 cp lambda.zip s3://$AWS_S3_BUCKET_NAME/${PROJECT_NAME}-${ENVIRONMENT}-${FUNCTION_NAME_PART}.zip --profile ${PROFILE}
 
 #aws lambda update-function-code --function-name ${FUNCTION_NAME}  --zip-file fileb://lambda.zip --profile ${PROFILE}
 
