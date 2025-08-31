b93bb8b4fe037cc8cfa5c18ffbf6ecb92bdb9bdf
Krzysztof Józwiak <krzysztof@itsnap.eu>
2025-08-31 19:10:14 +0200

    chore: update function names for clarity and improve zip packaging script with temp directory usage

--- infra/aws/serverless/zip_to_s3.sh
+++ infra/aws/serverless/zip_to_s3.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+set -x
 
 source ./env.sh
 
@@ -31,7 +32,30 @@ while IFS= read -r FUNCTION_NAME; do
   LAMBDA_NAME="${PROJECT_NAME}-${ENVIRONMENT}-${FUNCTION_NAME}"
   echo "function3: >$LAMBDA_NAME<"
 
-  zip -r "${LAMBDA_NAME}.zip" "../lambdas/${FUNCTION_NAME}"
+#  zip -r "${LAMBDA_NAME}.zip" "../lambdas/${FUNCTION_NAME}"
+# wrong
+#  (cd "../lambdas/${FUNCTION_NAME}" && zip -r "../../tmp/${LAMBDA_NAME}.zip" .)
+
+  TEMP_FUNCTION_DIR="${LAMBDA_NAME}_temp"
+  mkdir -p "${TEMP_FUNCTION_DIR}"
+
+  # Skopiuj zawartość katalogu funkcji do tymczasowego katalogu
+  cp -r "../lambdas/${FUNCTION_NAME}/"* "${TEMP_FUNCTION_DIR}/"
+
+  # Spakuj zawartość tymczasowego katalogu
+  cd ${TEMP_FUNCTION_DIR}
+  pwd
+  ls -l
+  zip -r "${LAMBDA_NAME}.zip" "./"*
+
+  cd ..
+  ls
+  mv ${TEMP_FUNCTION_DIR}/*.zip ./
+
+  ls
+
+  # Usuń tymczasowy katalog
+  rm -rf "${TEMP_FUNCTION_DIR}"
 
   # Wysyłanie pliku zip na S3
   aws s3 cp "${LAMBDA_NAME}.zip" "s3://${AWS_S3_BUCKET_NAME}/${LAMBDA_NAME}.zip"
